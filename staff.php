a51aae9aed0aeeb69f81657cad5bf1203e997d8d
Ravi Nishad <97040275+ravinishadcoder@users.noreply.github.com>
2023-01-11 15:51:38 +0530

    added css to staff.php file

--- staff.php
+++ staff.php
@@ -4,6 +4,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Staff</title>
+    <link href="stylesheets/public.css" media="all" rel="stylesheet" type="text/css" />
 </head>
 <body>
 <body>
