75da99845e81a1049c0bd6b9949be6db2866f3e7
Ravi Nishad <97040275+ravinishadcoder@users.noreply.github.com>
2023-01-11 15:42:24 +0530

    completed staff file

---
+++ staff.php
@@ -0,0 +1,34 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Staff</title>
+</head>
+<body>
+<body>
+		<div id="header">
+			<h1>Widget Corp</h1>
+		</div>
+		<div id="main">
+			<table id="structure">
+				<tr>
+					<td id="navigation">
+						&nbsp;
+					</td>
+					<td id="page">
+						<h2>Staff Menu</h2>
+						<p>Welcome to the staff area.</p>
+						<ul>
+							<li><a href="content.php">Manage Website Content</a></li>
+							<li><a href="new_user.php">Add Staff User</a></li>
+							<li><a href="logout.php">Logout</a></li>
+						</ul>
+					</td>
+				</tr>
+			</table>
+		</div>
+		<div id="footer">Copyright 2007, Widget Corp</div>
+	</body>
+</body>
+</html>
