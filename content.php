8948f29f375f1faed9a2e9c5311a9a8cb53ddae0
Ravi Nishad <97040275+ravinishadcoder@users.noreply.github.com>
2023-01-12 11:29:50 +0530

    added dynamic content in content.php file

--- content.php
+++ content.php
@@ -1,5 +1,17 @@
 <?php require_once("includes/connection.php"); ?>
 <?php require_once("includes/functions.php"); ?>
+<?php
+if(isset($_GET["subj"])){
+	$sel_subj=$_GET["subj"];
+    $sel_page="";
+}elseif(isset($_GET["page"])){
+	$sel_page=$_GET["page"];
+	$sel_subj="";
+}else{
+	$sel_page="";
+	$sel_subj="";
+}
+?>
 <?php include("includes/header.php"); ?>
 <table id="structure">
 	<tr>
@@ -10,14 +22,16 @@
 		if ($subject_result->num_rows > 0) {
 			// output data of each row
 			while($subject = $subject_result->fetch_assoc()) {
-			  echo "<li>{$subject["menu_name"]}</li>"  ;
+			  echo "<li><a href=\"content.php?subj=".urlencode($subject["id"])."\">{$subject["menu_name"]}</a></li>"  ;
 			  echo "<ul class=\"pages\">";
 			  
 			$result=get_pages_for_subject($subject["id"]);
 		if ($result->num_rows > 0) {
 			// output data of each row
 			while($row = $result->fetch_assoc()) {
-			  echo "<li>{$row["menu_name"]}</li>"  ;
+			  echo "<li>
+			  <a href=\"content.php?page=".urlencode($row["id"])."\">
+			  {$row["menu_name"]} </a></li>"  ;
 			}
 			echo"</ul>";
 		  }
@@ -28,7 +42,12 @@
 		</td>
 		<td id="page">
 			<h2>Content Area</h2>
-			
+			<?php
+			echo $sel_subj;
+			?><br>
+			<?php
+			echo $sel_page;
+			?><br>
 		</td>
 	</tr>
 </table>
