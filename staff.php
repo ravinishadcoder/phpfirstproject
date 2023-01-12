d66a6f34036a46c3b2a123c82a25d41edcb70350
Ravi Nishad <97040275+ravinishadcoder@users.noreply.github.com>
2023-01-12 19:20:43 +0530

    added login.php file

--- staff.php
+++ staff.php
@@ -1,3 +1,4 @@
+<?php require_once("includes/session.php"); ?>
 <?php include("includes/header.php"); ?>
 			<table id="structure">
 				<tr>
@@ -6,7 +7,7 @@
 					</td>
 					<td id="page">
 						<h2>Staff Menu</h2>
-						<p>Welcome to the staff area.</p>
+						<p>Welcome to the staff area:<?php echo $_SESSION['username'] ?></p>
 						<ul>
 							<li><a href="content.php">Manage Website Content</a></li>
 							<li><a href="new_user.php">Add Staff User</a></li>
